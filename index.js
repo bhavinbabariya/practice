dc70844e0255068df53e9dd9f2584620cc0a3ed8
Bhavin Babariya <bhavin.b@simformsolutions.com>
2023-02-17 15:52:34 +0530

    promise part added

--- index.js
+++ index.js
@@ -10,3 +10,27 @@ const obj = {
 
 obj.func1();
 obj.func2();
+
+try {
+    const p1 = Promise.resolve();
+    // throw new Error("hello 454545");
+    const p2 = Promise.reject("hello there is error");
+
+    // const p2 = new Promise((resolve, reject) => reject("hello there is error"));
+    const p4 = Promise.resolve();
+    const p3 = Promise.resolve();
+    Promise.all([p1, p4, p3])
+        .then(() => {
+            console.log("SUCESSS");
+        })
+        .catch((err) => {
+            console.log("ERROR", err);
+        });
+
+    // p2.catch((err) => {
+    //     console.log(err);
+    // });
+} catch (err) {
+    console.log("err : ", err);
+}
+console.log("END");
