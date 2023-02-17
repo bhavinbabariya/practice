a066f1991458ee3429be92e85b6d733ec26ef809
meetvaishnani5291 <meet.vaishnani@simformsolutions.com>
2023-02-17 16:18:24 +0530

    file2 added.

---
+++ file2.js
@@ -0,0 +1,17 @@
+let fun2 = () => {
+  console.log(this);
+};
+
+class MyFirstClass {
+  fun1 = fun2;
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  calc(scs) {
+    console.log('hii');
+  }
+}
+new MyFirstClass().fun1();
